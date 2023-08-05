3c3adbe71825ec440ae4c2552e002829d5f23ef3
xxudon <ahlinder.leo@icloud.com>
2023-08-05 17:22:31 +0200

    latest

---
+++ react-app/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const path = require('path');
+const mysql = require("mysql");
+const { error } = require("console");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'lightnovelonline',
+  });
+
+
+  connection.connect((error) =>{
+    if (error){
+        console.log("error",error)
+        return;
+    }
+    else{
+        console.log("SUCUESSS")
+    }
+  })
+
+
+app.get("/api/profile",function(error,results){
+  console.log("SUCESS")
+})
+  
--- react-app/src/header/header.js
+++ react-app/src/header/header.js
@@ -2,6 +2,23 @@ import React from 'react';
 import "./headerStyle.css"
 
 const Header = () =>{
+
+    const profileButtonClicked = async () =>{
+        try{
+            const respone = await fetch("/api/profile",{
+                method:"GET",
+                headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }});
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+
+
     return(
         <div className="headerDesign">
             <div className="header">
@@ -9,7 +26,7 @@ const Header = () =>{
                 <button className='button'>Best Selling</button>
                 <button className='button'>Create</button>
                 <button className='button'>Search</button>
-                <button className='profileButton'>Profile</button>
+                <button className='profileButton' onClick={profileButtonClicked}>Profile</button>
                 <button className='profileButton'>Libary</button>
             </div>
         </div>
