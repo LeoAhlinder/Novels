208ddccc2d1e75785d60bc48e94909695b6055d8
xxudon <ahlinder.leo@icloud.com>
2023-08-07 11:00:47 +0200

    working profile

--- react-app/server.js
+++ react-app/server.js
@@ -4,10 +4,19 @@ const bodyParser = require("body-parser")
 const path = require('path');
 const mysql = require("mysql");
 const { error } = require("console");
+const cors = require('cors');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Allow only requests from a specific domain
+const corsOptions = {
+  origin: 'http://localhost:3000  ',
+};
+
+app.use(cors(corsOptions));
+
 
 
 const port = process.env.PORT || 3001
@@ -36,6 +45,8 @@ const connection = mysql.createConnection({
 
 
 app.get("/api/profile",function(req,res){
-  res.json({message:"YUP"})
+
+  const test = 2
+  res.json({message:test})
 })
   
--- react-app/src/Profile/Profile.js
+++ react-app/src/Profile/Profile.js
@@ -9,21 +9,21 @@ const Profile = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch("/api/profile", {
+                const res = await fetch("http://localhost:3001/api/profile", {
                     method: "GET",
                     headers: {
                         'Content-Type': 'application/json'
                     }
                 });
                 const response = await res.json();
-                console.log(response.message)
+                //console.log(response.message)
                 if (!response.ok) {
                     console.log(response);
                 } else {
                     console.log(response.message)
                 }
             } catch (err) {
-                console.log(err);
+                console.log("error"+err);
             }
         };
 
