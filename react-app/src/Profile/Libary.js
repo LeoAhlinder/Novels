87d11434a0aad14b8ca93ad194aa37ff1ec3cdf8
xxudon <ahlinder.leo@icloud.com>
2023-08-08 09:33:48 +0200

    latest

--- react-app/src/Profile/Libary.js
+++ react-app/src/Profile/Libary.js
@@ -3,18 +3,26 @@ import "./Libary.css"
 
 const Libary = (bookIds) =>{
 
+
     useEffect(()=>{
         //Fetch Bookids from User
     })
 
     return(
       <div className="Libary">
+        <li className="BookList">
+
+        </li>
         LIBARY
       </div>  
     );
     
-
     
 }
 
+//Logo,Title,TotalPages,CurrentPage
+Libary()
+
+
 export default Libary;
+
