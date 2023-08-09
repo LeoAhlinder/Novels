c7fe4306262b19b8aa366be21176b574220c1f96
xxudon <ahlinder.leo@icloud.com>
2023-08-09 10:17:25 +0200

    show all the books

--- react-app/src/Profile/Libary.jsx
+++ react-app/src/Profile/Libary.jsx
@@ -1,43 +1,46 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 import "./Libary.css"
 
-const Library = (userId) => {
+const Library = () => {
+  const [books, setBooks] = useState([]);
+
   useEffect(() => {
     const userLibrary = async (userId) => {
       try {
-        // Fetch Bookids from User
         const res = await fetch(`http://localhost:3001/api/library/${userId}`, {
           method: "GET",
-          headers: 
-          { "Content-Type": "application/json",
-          'Accept': 'application/json', }
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
         });
 
-        if (res.ok){
+        if (res.ok) {
           const response = await res.json();
-          console.log(response.data)
+          // Assuming your response.data contains the array of books
+          setBooks(response.data);
+        } else {
+          console.log("error");
         }
-        else{
-          console.log("error")
-        }
-
-
       } catch (err) {
         console.log(err);
       }
     };
 
-    userLibrary(1);
-  });
+    userLibrary(1); // Replace 1 with the actual userId
+  }, []);
 
-
-    return(
-      <div className="Libary">
-        <ul>
-          <li className="BookList">TESTNNN</li>
-        </ul>
-      </div>  
-    );    
+  return (
+    <div className="Library">
+      <ul>
+        {books.map((book) => (
+          <li className="BookList" key={book.bookid}>
+            {book.title} - Page: {book.currentPage}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 //Logo,Title,TotalPages,CurrentPage
