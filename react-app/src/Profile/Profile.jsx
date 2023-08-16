ef84689a616926ce6f89af9317b43f1bdd4ab73e
xxudon <ahlinder.leo@icloud.com>
2023-08-16 20:52:25 +0200

    profile button see

--- react-app/src/Profile/Profile.jsx
+++ react-app/src/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../Profile/profile.css"
 import Library from "./Libary"
 import { useNavigate } from "react-router-dom"
@@ -17,16 +18,27 @@ const Profile = () => {
 
     const userName = localStorage.getItem("userName")
 
+    const [selected,changeSelected] = useState("0")
+  
+    const info = () =>{
+        changeSelected("1")
+    }
+    const NovelCreated = () =>{
+        changeSelected("2")
+    }
+    const Comments = () =>{
+        changeSelected("3")
+    }
+  
     return (
         <div>
             <div className='profileHeader'>
                 <h1 className='profileHeader'>Profile Page</h1>
                 <button onClick={logOut} className='logout'>Log Out</button>
                 <p className='userName'>Welcome, {userName}</p>
-                <button className="InfoButton">Info</button>
-                <button className="TestButton">test</button>
-                <button className="YesButton">test5</button>
-
+                <button onClick={info}className={selected === "1" ? "InfobuttonClicked" : "InfoButton"}>Info</button>
+                <button onClick={NovelCreated}className={selected === "2" ? "NovelsCreatedButtonClicked" : "NovelsCreated"}>Novels Created</button>
+                <button onClick={Comments} className={selected === "3" ? "CommentsbuttonClicked" : "Comments"}>Comments</button>
             </div>
             <div>
                 <Library/>
