ec85bba309ed6f00dc5a318aad36235b8d26568b
xxudon <ahlinder.leo@icloud.com>
2023-08-17 10:34:26 +0200

    home pic fixed

--- react-app/src/Home/Home.jsx
+++ react-app/src/Home/Home.jsx
@@ -5,23 +5,13 @@ import LibraryPicMedium from "../Pictures/librarypic_850x250.jpg";
 import LibraryPicSmall from "../Pictures/librarypic_1_650x200.jpg";
 import LibraryPicSmaller from "../Pictures/librarypic_2_450x175.jpg";
 import LibraryPicMini from "../Pictures/librarypic_3_275x100.jpg";
-import LibraryPicMicro from "../Pictures/librarypic_4_150x75.jpg";
+import LibraryPicMicro from "../Pictures/librarypic_250x270.jpg";
 
 const Home = () =>{
     return(
         <div>
             <div className="homeIntro">
-                <picture className="LibraryPic">
-                <source media="(min-width: 1400px)" srcset={LibraryPicBig}/>
-                <source media="(min-width: 900px)"srcset={LibraryPicMedium}/>
-                <source media="(min-width: 750px)"srcset={LibraryPicSmall}/>
-                <source media="(min-width: 750px)"srcset={LibraryPicSmall}/>
-                <source media="(min-width: 525px)"srcset={LibraryPicSmaller}/>
-                <source media="(min-width: 300px)"srcset={LibraryPicMini}/>
-
-
-                <img src={LibraryPicMicro} alt="" />
-                </picture>
+                <img src={LibraryPicBig} alt="" />
             </div>
             <div className="homeWrapper">
 
