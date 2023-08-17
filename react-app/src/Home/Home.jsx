9658b7300e25b9efbf10f764064a2fd759884de7
xxudon <ahlinder.leo@icloud.com>
2023-08-17 11:05:43 +0200

    grid for home

--- react-app/src/Home/Home.jsx
+++ react-app/src/Home/Home.jsx
@@ -13,10 +13,22 @@ const Home = () =>{
             <div className="homeIntro">
                 <img src={LibraryPicBig} alt="" />
             </div>
-            <div className="homeWrapper">
-
-                <p>New release</p>
-            </div>
+            <ul className="homeWrapper">
+            <ul className="grid-container">
+                <li className="grid-item">1</li>
+                <li className="grid-item">2</li>
+                <li className="grid-item">3</li>  
+                <li className="grid-item">4</li>
+                <li className="grid-item">5</li>
+                <li className="grid-item">6</li>  
+                <li className="grid-item">7</li>
+                <li className="grid-item">8</li>
+                <li className="grid-item">9</li>  
+                <li className="grid-item">10</li>  
+                <li className="grid-item">11</li>  
+                <li className="grid-item">12</li>  
+            </ul>
+            </ul>
         </div>
     )
 }
