206aafb40fc5dc3bd452a6b8b9dae22389cc231b
xxudon <ahlinder.leo@icloud.com>
2023-08-24 19:13:43 +0200

    dashboard for create

--- react-app/src/Create/create.jsx
+++ react-app/src/Create/create.jsx
@@ -11,13 +11,17 @@ const Create = () =>{
     }
 
     return(
-        <div className="sideBar">
-            <h1 >Dashboard</h1>
-            <button className="button">WorkSpace</button>
-            <br />
-            <button className="button" onClick={CreateNew}>New Novel</button>
+        <div>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <div className="sideNav">
+                <h1>Dashboard</h1>
+                <button className="button">WorkSpace</button>
+                <button className="button" onClick={CreateNew}>New Novel</button>
 
+            </div>
         </div>
+
+    
     );
 }
 
