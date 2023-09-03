6c7de8bd828e37903ec0b8b4f47162308a228a06
xxudon <ahlinder.leo@icloud.com>
2023-09-03 19:37:24 +0200

    check bookinfo client

--- react-app/src/Create/createNew.jsx
+++ react-app/src/Create/createNew.jsx
@@ -5,7 +5,10 @@ const CreateNew = () =>{
 
     const [bookName,changeBookName] = useState("")
     const [summary,changeSummary] = useState("")
-
+    const [genre,setGenre] = useState("")
+    const [language,changeLanguage] = useState("")
+    const [tags,changeTags] = useState("")
+    const [warnings,changeWarnings] = useState("")
 
     const handleInputChangeBookName = (e) => {
         changeBookName(e.target.value)
@@ -15,54 +18,93 @@ const CreateNew = () =>{
         changeSummary(e.target.value)
     }
 
+    const handleLanguage = (e) =>{
+        changeLanguage(e.target.value)
+    }
+
+    const handleGenreChange = (e) =>{
+        setGenre(e.target.value)
+    }
+
+    const handleWarnings = (e) =>{
+        changeWarnings(e.target.value)
+    }
+
+    const handleCustomTags = (e) =>{
+        changeTags(e.target.value)
+    }
+
+    const checkBookInfo = () =>{
+        const bookInfo = {
+            Title: bookName,
+            Synopsis:summary,
+            Genre:genre,
+            Language:language,
+            Tags:tags,
+            Warnings:warnings,
+        }
+        console.log(bookInfo)
+    }
+
     return(
     <>
         <div className="CreateNovelsContainer"> 
             <div className="bookContent" >
 
-                <label htmlFor="bookName" className="Label">Book Name {bookName.length}/20</label>
-                <input type="text" className="bookNameInput" maxLength="20"placeholder="20 letters max" onChange={handleInputChangeBookName}/>
-                <label htmlFor="" className="Label">Synopsis {summary.length}/750</label>
-                <textarea type="text" name="" placeholder="Make a awesome synopsis to attract viewers!" maxLength="750" className="summaryInput" onChange={handleSummaryChange}/>
-                <label htmlFor="" className="Label">Genre</label>
-                <select name="Genre" className="Selector" id="genreSelect">
-                    <option>Select</option>
-                    <option>Mystery</option>
-                    <option>Romance</option>
-                    <option>Science Fiction</option>
-                    <option>Fantasy</option>
-                    <option>Thriller</option>
-                    <option>Historical Fiction</option>
-                    <option>Non-Fiction</option>
-                    <option>Biography</option>
-                    <option>Self-Help</option>
-                    <option>Horror</option>
-                    <option>Adventure</option>
-                    <option>Dystopian</option>
-                    <option>Young Adult</option>
-                    <option>Memoir</option>
-                    <option>Comedy</option>
-                    <option>Sport</option>
-                    <option>Games</option>
+                <label htmlFor="title" className="Label">Book Name {bookName.length}/20</label>
+                <input type="text" className="bookNameInput" maxLength="20"placeholder="20 letters max" id="title" onChange={handleInputChangeBookName}/>
+                <label htmlFor="summary" className="Label">Synopsis {summary.length}/750</label>
+                <textarea type="text" id="summary" placeholder="Make a awesome synopsis to attract viewers!" maxLength="750" className="summaryInput" onChange={handleSummaryChange}/>
+                <label htmlFor="genreSelect" className="Label">Genre</label>
+
+                <select 
+                    name="Genre" 
+                    className="Selector" 
+                    id="genreSelect" 
+                    value={genre} 
+                    onChange={handleGenreChange}
+                >
+                    <option value="Select">Select</option>
+                    <option value="Mystery">Mystery</option>
+                    <option value="Romance">Romance</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Historical Fiction">Historical Fiction</option>
+                    <option value="Non-Fiction">Non-Fiction</option>
+                    <option value="Biography">Biography</option>
+                    <option value="Self-Help">Self-Help</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Dystopian">Dystopian</option>
+                    <option value="Young Adult">Young Adult</option>
+                    <option value="Memoir">Memoir</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Sport">Sport</option>
+                    <option value="Games">Games</option>
                 </select>
 
-                <label htmlFor="" className="Label">Language</label>
-                <input type="text" className="bookNameInput" placeholder="Language" maxLength="25"/>
+                <label htmlFor="language" className="Label">Language</label>
+                <input type="text" id="language" className="bookNameInput" placeholder="Language" maxLength="25" onChange={handleLanguage}/>
 
-                <label htmlFor="" className="Label">Custom Tags</label>
-                <input type="text" className="bookNameInput" placeholder="Tags are seperated by spaces. ex 'tag1 tag2 tag3-tag3'" />
+                <label htmlFor="tags" className="Label">Custom Tags 0/10</label>
+                <input type="text" id="tags" onChange={handleCustomTags} className="bookNameInput" placeholder="Tags are seperated by spaces. ex 'tag1 tag2 tag3-tag3'" />
 
-                <label htmlFor="" className="Label">Warnings</label>
-                <select name="Warnings" className="Selector">
+                <label htmlFor="warnings" className="Label">Warnings</label>
+                <select 
+                    name="Warnings" 
+                    className="Selector" 
+                    value={warnings} 
+                    id="warnings"
+                    onChange={handleWarnings}
+                    >
                     <option value="">Select</option>
-                    <option value="">For everyone</option>
-                    <option value="">PG-13</option>
-                    <option>Guardian guidance recommended</option>
-                    <option>18+</option>
+                    <option value="For everyone">For everyone</option>
+                    <option value="PG-13">PG-13</option>
+                    <option value="Guardian guidance recommended">Guardian guidance recommended</option>
+                    <option value="18+">18+</option>
                 </select>
-
-                <button className="publishButton">Publish now</button>
-
+                <button className="publishButton" onClick={checkBookInfo}>Publish now</button>
             </div>
         </div>
     </>
