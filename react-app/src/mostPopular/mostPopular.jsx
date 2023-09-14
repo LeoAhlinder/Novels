61f961c07a8dfeaccaad8e5a9e7d7b7ed5c68d95
xxudon <ahlinder.leo@icloud.com>
2023-09-14 21:49:28 +0200

    most popular updates

--- react-app/src/mostPopular/mostPopular.jsx
+++ react-app/src/mostPopular/mostPopular.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./mostPopularStyle.css"
+import cat from "../Pictures/coolcat.jpg"
+
 const MostPopular = () =>{
+
+    const overallRanking = () =>{
+        console.log("w")
+    }
+
+
     return(
         <>
-         <div class="containerPopular">
-            <div class="categories">
-                <div class="button-container">
-                    <button class="category-button">Overall Ranking</button>
-                    <button class="category-button">Collections</button>
-                    <button class="category-button">Rating</button>
+         <div className="containerPopular">
+            <div className="categories">
+                <div className="button-container">
+                    <button className="category-button" onClick={overallRanking}>Overall Ranking</button>
+                    <button className="category-button">Collections</button>
+                    <button className="category-button">Rating</button>
                 </div>
+                <ul className="books">
+                    <li> <img src={cat} style={{height:100}} alt="" /></li>
+                </ul>
             </div>
         </div>
         </>
