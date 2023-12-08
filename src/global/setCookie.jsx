2a1681c5cd559aee409174bd9a7000ffadb4861b
Leo Ahlinder <59998695+LeoAhlinder@users.noreply.github.com>
2023-12-08 19:27:28 +0100

    Delete src/global directory

--- src/global/setCookie.jsx
+++
@@ -1,8 +0,0 @@
-const setCookie = (name,value,hours) =>{
-    let expiration = new Date();
-    expiration.setTime(expiration.getTime() + (hours * 60 * 60 * 1000)); // Convert hours to milliseconds
-    let expires = "expires=" + expiration.toUTCString();
-    document.cookie = name + "=" + JSON.stringify(value) + "; " + expires;
-}
-
-export default setCookie
