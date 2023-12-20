800dd4b901d59c9d4fb8b82df9c43904b5471f0e
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-12-20 15:39:56 +0100

    Add choosing chapter title

--- src/Components/novelWorkspace/createNewChapter.jsx
+++ src/Components/novelWorkspace/createNewChapter.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-const CreateChapter = ({chapterContentLength,handleTextAreaLength,publishChapter,setConfirmation,confirmation}) =>{
+const CreateChapter = ({chapterContentLength,handleTextAreaLength,publishChapter,setConfirmation,confirmation,handleTitleChange}) =>{
     return(
         <>
-        <h2 id="maxChaptersText">{chapterContentLength}/35000</h2>
-        <textarea onChange={handleTextAreaLength} type="text" maxLength={35000} id="novelWorkspaceChapterInputText"/>
+        <input type="text" placeholder="Title" id="novelWorkspaceChapterTitleInput" onChange={handleTitleChange} />
+        <h2 id="maxChaptersText">{chapterContentLength}/100000</h2>
+        <textarea onChange={handleTextAreaLength} type="text" maxLength={100000} id="novelWorkspaceChapterInputText"/>
         {confirmation ? 
         <div>
           <button className="novelWorkshopPublishChapterButton" id="confirmButton" onClick={publishChapter}>
