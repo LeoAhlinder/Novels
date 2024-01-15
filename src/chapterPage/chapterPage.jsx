00d07331768ab23f0ba60a50cb018cd8c59eaf00
Leo Ahlinder <ahlinder.leo@icloud.com>
2024-01-15 14:56:48 +0100

    Add API request for getting chapter info

--- src/chapterPage/chapterPage.jsx
+++ src/chapterPage/chapterPage.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { get } from "../../server";
 
 const ChapterPage = () => {
+
+
+    useEffect(()=>{
+        const getChapterInfo = async () =>{
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/api/chapterInfo`,{
+                method:"GET",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            const response = await res.json();
+
+            console.log(response)
+        }
+
+        getChapterInfo();
+    },[])
+
+
     return(
         <div>
             <h1>Chapter Page</h1>
