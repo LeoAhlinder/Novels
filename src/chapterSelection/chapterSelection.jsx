057b919c4453c828afbb2ae01ed37616d7f0e2b3
Leo Ahlinder <ahlinder.leo@icloud.com>
2024-01-26 10:49:10 +0100

    Remove unsued import

--- src/chapterSelection/chapterSelection.jsx
+++ src/chapterSelection/chapterSelection.jsx
@@ -2,7 +2,6 @@ import React, {useEffect,useState} from "react";
 import "./chapterSelectionStyle.css"
 
 import ChangeDocumentTitle from "../Global/changeDocumentTitle";
-import Cookies from "js-cookie";
 
 import { useNavigate } from "react-router";
 
