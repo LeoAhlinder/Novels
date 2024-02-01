d468b829d179f8074fb1632356b581c1cb00c155
Leo Ahlinder <ahlinder.leo@icloud.com>
2024-02-01 09:32:27 +0100

    Add terms of service

--- src/termsOfService/termsOfService.jsx
+++ src/termsOfService/termsOfService.jsx
@@ -28,7 +28,7 @@ const TermsOfService = () => {
 
                 3. User Responsibility and Conduct:
                 <br />
-                - As a user, you are responsible for your actions. Failure to follow the rules or posting inappropriate content may result in a ban from the website without warning.
+                - As a user, you are responsible for your actions. Failure to follow the <a href="/rules" target="_blank" id="rulesTag">rules</a> or posting inappropriate content may result in a ban from the website without warning.
 
                 <br /><br />
 
@@ -46,14 +46,14 @@ const TermsOfService = () => {
                 7. Copyright and DMCA Notice:   
                 <br />
                 - We respect the intellectual property rights of others. If you believe that your work has been used on our website in a way that constitutes copyright infringement, please provide our designated agent with the following information:
-                
+                    <br />
                     - A physical or electronic signature of the copyright owner or a person authorized to act on their behalf.
                     <br />
                     - Identification of the copyrighted work claimed to have been infringed.
                     <br />
                     - Identification of the material that is claimed to be infringing or to be the subject of infringing activity.
                     <br />
-                    - Contact information for the copyright owner, such as an address, telephone number, or email address.
+                    - Contact information for the copyright owner, such as email address.
                     <br />
                     - A statement that the complaining party has a good faith belief that the use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.
                     <br />
@@ -64,14 +64,12 @@ const TermsOfService = () => {
                 8. Counter-Notification:
                 <br />
                 - If you believe that your content was mistakenly removed or disabled due to a misidentification or error, you may submit a counter-notification. The counter-notification must include:
-                
-                    - Your physical or electronic signature.
                     <br />
                     - Identification of the material that has been removed or to which access has been disabled. 
                     <br />
                     - A statement under penalty of perjury that you have a good faith belief that the material was removed or disabled as a result of mistake or misidentification.
                     <br />
-                    - Your name, address, and telephone number, and a statement that you consent to the jurisdiction of the federal district court for the judicial district in which your address is located, or if your address is outside of the United States, for any judicial district in which the service provider may be found, and that you will accept service of process from the person who provided the original notification of infringement.
+                    - Your username, email address and a statement that you consent to the jurisdiction of the federal district court for the judicial district in which your address is located, or if your address is outside of the United States, for any judicial district in which the service provider may be found, and that you will accept service of process from the person who provided the original notification of infringement.
 
                 <br /><br />
 
