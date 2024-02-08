6d4c05bdf3ef78d0aac12d06a33746234c6bef76
xxudon <ahlinder.leo@icloud.com>
2024-02-08 21:28:53 +0100

    Set cookieform on whole screen

--- src/Components/CookiesForm/cookiesForm.jsx
+++ src/Components/CookiesForm/cookiesForm.jsx
@@ -7,10 +7,14 @@ import "./cookiesFormStyle.css"
 const CookiesForm = () => {
 
     const [collectCookiesFormShow, setCollectCookiesFormState] = useState(false);
+    const [cookiesSelectionState, setCookiesSelectionState] = useState(false);
 
     const storeCookies = (e) =>{
         if (e.target.innerText === "Accept All"){
             setCookie("cookiesAccepted",true,365)
+        }
+        else{
+            setCookie("cookiesAccepted",false,7)
         }
         setCollectCookiesFormState(false)
     }
@@ -26,20 +30,28 @@ const CookiesForm = () => {
 
     return (
         <div id={collectCookiesFormShow === false ? "doNotShow" : "cookieForm"}>
-            <div id="cookieFormContent">
-                <p id="cookieTitle">We respect your privacy</p>
-                <div id="cookiesInfoText">
-                    Cookies are used for remembering your preferences for the website and storing books in most popular, aswell as storing your authentication token to keep you logged in.
-                    The cookies are not used by any third party and are only used by Novels.com to make your experience better.
-                    <br />
-                    <br />
-                    By clicking <span id="acceptAllText">"Accept All"</span> you accept that Novels.com will set cookies.
-                    <div id="cookieBannerButtons">
-                        <button className="bannerButton" onClick={storeCookies}>Accept All</button>
-                        <button className="bannerButton" onClick={storeCookies}>Reject</button>
-                    </div>
-                </div>    
-            </div>
+            <div id="cookieBanner">
+                <div id="cookieFormContent">
+                    <p id="cookieTitle">We respect your privacy</p>
+                    <div id="cookiesInfoText">
+                        Cookies are used for remembering your preferences for the website and storing books from the most popular.
+                        The cookies are not used by any third party and are only used by Novels.se to make your experience better.
+                        <br />
+                        <br />
+                        By clicking <span id="acceptAllText">"Accept All"</span> you accept that Novels.se will set cookies.
+                        <div id="cookieBannerButtons">
+                            <button className="bannerButton" id="AcceptButton" onClick={storeCookies}>Accept All</button>
+                            <button className="bannerButton" id="RejectButton" onClick={storeCookies}>Reject</button>
+                            <button className="bannerButton" onClick={() => setCookiesSelectionState(true)}>Adjust</button>
+                        </div>
+                    </div>    
+                </div>
+                {cookiesSelectionState === true ? 
+            <>
+                <p>test</p>
+            </> 
+            : ""}
+            </div> 
         </div>
     )
 }   
