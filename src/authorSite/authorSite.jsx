2076dd60dc03ade07d1edf4c28b43c4f30ff676b
xxudon <ahlinder.leo@icloud.com>
2024-02-25 13:36:39 +0100

    Change chapters to singular when one chapter

--- src/authorSite/authorSite.jsx
+++ src/authorSite/authorSite.jsx
@@ -86,7 +86,7 @@ const AuthorSite = () =>{
             authorInfo.map((book,index) => (
                 <li key={index} className="authorBookItem" >
                     <h3 id="Title" onClick={() => goToBook(book)}>{book.title}</h3>
-                    <h4 className="bookInfo" id="Chapters">{book.totalpages != null ? book.totalpages : "0"} Chapters</h4>
+                    <h4 className="bookInfo" id="Chapters">{book.totalpages != null ? book.totalpages : "0"} {book.totalpages === 1 ? "Chapter" : "Chapters"}</h4>
                     <h4 className="bookInfo" id="totalInLibrary">{book.totalinlibrary === null ? 0 : book.totalinlibrary} {book.totalinlibrary === 1 ? "Bookmark" : "Bookmarks"}</h4>
                     <h4 className="bookInfo" id="Genre">Genre: {book.genres}</h4>
                     <h4 className="bookInfo" id="PR" >Age limit: {book.warnings}</h4>
