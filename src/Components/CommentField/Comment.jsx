c75ee9776e54b8774d057a619ff14a8e0019b4c1
xxudon <ahlinder.leo@icloud.com>
2024-03-18 19:38:14 +0100

    Update username is "Deleted user" if user is deleted

--- src/Components/CommentField/Comment.jsx
+++ src/Components/CommentField/Comment.jsx
@@ -173,7 +173,11 @@ const Comment = ({replies,bookid,id, dislikes: initialDislikes, likes: initialLi
                 {replies.length > 0 ? replies.map((reply, index) =>
                     reply.relatedTo === id ?  
                     <div className="Reply" key={index}>
-                        <h3 className={reply.userName === viewingUser ? "replyUser thisUserComment" : "replyUser"}>{reply.userName === viewingUser ? "You" : reply.userName}</h3>
+
+                        <h3 onClick={reply.userName === null ? () => goToAuthorSite(reply.userName) : null} className={reply.userName === viewingUser ? "replyUser thisUserComment" : "replyUser"}>
+                            {reply.userName === null ? "Deleted User" : reply.userName === viewingUser ? "You" : reply.userName}
+                        </h3>
+
                         <p className="replyText">{reply.comment}</p>
                         {
                             reply.userName === viewingUser ?
